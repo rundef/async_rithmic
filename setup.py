1bdcd2f0ecc0ce85e4a484d077efebd511146d91
rundef <bash88@gmail.com>
2024-10-21 13:31:42 -0400

    Bump version: 0.1.0 → 1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='async_rithmic',
-    version='0.1.0',
+    version='1.0.0',
     author='Mickael Burguet',
     description='Python API Integration with Rithmic Protocol Buffer API',
     keywords='python rithmic',
--- src/rithmic/__init__.py
+++ src/rithmic/__init__.py
@@ -2,4 +2,4 @@ from rithmic.client import RithmicClient
 from rithmic.enums import *
 from rithmic.logger import logger
 
-__version__ = '0.1.0'
+__version__ = '1.0.0'
