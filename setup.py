f6b33e0532eae5e9958801a7a6be42eda112e4f5
rundef <bash88@gmail.com>
2024-11-10 19:08:31 -0500

    Bump version: 1.2.0 → 1.2.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='async_rithmic',
-    version='1.2.0',
+    version='1.2.1',
     author='Mickael Burguet',
     description='Python API Integration with Rithmic Protocol Buffer API',
     keywords='python rithmic',
