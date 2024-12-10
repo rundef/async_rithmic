efa7a3e7050364098c5b28907fd039d3f7c39d3f
rundef <bash88@gmail.com>
2024-12-10 17:40:30 -0500

    This should fix the failing test

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,6 +1,5 @@
 from unittest.mock import MagicMock, AsyncMock, patch
 from websockets import ConnectionClosedError
-from websockets.frames import Close
 from datetime import datetime
 import pytz
 
@@ -86,7 +85,7 @@ async def test_handle_reconnection():
     ])
 
     plant._recv.side_effect = [
-        ConnectionClosedError(rcvd=Close(code=1000, reason='Normal closure'), sent=Close(code=1000, reason='Normal closure')),
+        ConnectionClosedError(rcvd=None, sent=None),
         responses[0],
     ]
 
