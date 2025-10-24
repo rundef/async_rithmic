b26bae06815b91b5323dd4636ee41348fb1aec45
Brian DeCamp <brian@blackstonebay.com>
2025-10-24 14:21:47 -0600

    Added useful enums

--- async_rithmic/enums.py
+++ async_rithmic/enums.py
@@ -12,12 +12,14 @@ class DataType(enum.IntEnum):
 OrderType = pb.request_new_order_pb2.RequestNewOrder.PriceType
 OrderDuration = pb.request_new_order_pb2.RequestNewOrder.Duration
 TransactionType = pb.request_new_order_pb2.RequestNewOrder.TransactionType
+OrderPlacement = pb.request_new_order_pb2.RequestNewOrder.OrderPlacement
 
 LastTradePresenceBits = pb.last_trade_pb2.LastTrade.PresenceBits
 BestBidOfferPresenceBits = pb.best_bid_offer_pb2.BestBidOffer.PresenceBits
 OrderBookPresenceBits = pb.order_book_pb2.OrderBook.PresenceBits
 
 ExchangeOrderNotificationType = pb.exchange_order_notification_pb2.ExchangeOrderNotification.NotifyType
+ExchangeOrderBracketType = pb.exchange_order_notification_pb2.ExchangeOrderNotification.BracketType
 
 TimeBarType = pb.request_time_bar_replay_pb2.RequestTimeBarReplay.BarType
 
